fb6b4c19a12fc4f20af0016ad340f644ba5e2150
deniseglaus <denise.glaus@gmail.com>
2022-05-27 12:39:03 +0200

    added weather icon

--- Vanilla Weather app/src/index.js
+++ Vanilla Weather app/src/index.js
@@ -22,19 +22,24 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.main.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "f927e95d004267b11a157aabf2e3acc3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=46.204391&lon=6.143158&appid=${apiKey}&units=metric`;
