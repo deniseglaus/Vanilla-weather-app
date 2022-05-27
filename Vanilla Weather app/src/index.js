a6ed9b332ba92291d20d55f676c14a5389bcb95b
deniseglaus <denise.glaus@gmail.com>
2022-05-27 11:08:49 +0200

    added the current temperature

--- Vanilla Weather app/src/index.js
+++ Vanilla Weather app/src/index.js
@@ -1,1 +1,17 @@
-
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.main.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "f927e95d004267b11a157aabf2e3acc3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=46.204391&lon=6.143158&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
